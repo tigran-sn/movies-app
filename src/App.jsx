5ebd123278b9dc6ee070a939bc2155d614b84e1f
Tigran Soghomonyan <soghomonyantigran@gmail.com>
2025-08-08 01:40:24 +0400

    Implement movie fetching functionality in App component and add Spinner component for loading state

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Search from './components/Search'
+import Spinner from './components/Spinner'
+
+const API_URL = 'https://api.themoviedb.org/3/';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = `${API_URL}/discover/movie?include_adult=true&include_video=true&language=en-US&page=1&sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const movies = await response.json();
+      
+      if(movies.Response === 'False') {
+        setErrorMessage(movies.Error) || 'No movies found';
+        setMovieList([]);
+        return;
+      }
+
+      setMovieList(movies.results || []);
+      setLoading(false);
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Error fetching movies');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -18,6 +68,21 @@ const App = () => {
 
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
+        <section className='all-movies'>
+          <h2 className='mt-[40px]'>All Movies</h2>
+          {loading ? <Spinner /> : errorMessage ? <p className='text-red-500'>{errorMessage}</p> : (
+            <ul>
+              {movieList.map((movie) => (
+                <li key={movie.id}>
+                  <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+                  <h3>{movie.title}</h3>
+                  <p>{movie.overview}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </div>
     </main>
   )
